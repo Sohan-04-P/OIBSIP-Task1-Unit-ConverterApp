908354c49a5aba44867592986c842ea65350bd76
Sohan Patil <patilksohan28@gmail.com>
2023-07-15 16:23:01 -0700

    done

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.unitconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textv);
+        editText=findViewById(R.id.Edit_Txt);
+        button=findViewById(R.id.btn_one);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                int grams= 1000*kg;
+                textView.setText("The value after conversion"+grams);
+            }
+        });
+
+
+    }
+
+    }
